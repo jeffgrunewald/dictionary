27100abb2b9fef4b1c5299af4c20544b7c216ae1
jeffgrunewald <jeff@grunewalddesign.com>
2018-06-01 22:01:08 -0400

    First round piping to return random word selection

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -12,7 +12,12 @@ defmodule Dictionary do
       :world
 
   """
-  def hello do
-    :world
+  def random_word() do
+    Enum.random(word_list())
+  end
+
+  defp word_list do
+    File.read!("assets/words.txt")
+    |> String.split(~r/\n/)
   end
 end
